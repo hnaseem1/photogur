9d1c22055928ec3d043cc60a9c09a64b1fadaefb
Hasan Naseem <hn92@live.com>
2018-04-20 16:35:04 -0400

    login working

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
+
+  resource :sessions, only: %i(new create destroy)
+
+  resource :users, only: %i(new create)
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pictures#index'
-  get 'pictures' => 'pictures#index'
+  resources :pictures
 
-  post 'pictures' => 'pictures#create'
-  get 'pictures/new' => 'pictures#new'
-
-  get 'pictures/:id' => 'pictures#show'
-
-  get 'pictures/:id/edit' => 'pictures#edit'
-  patch 'pictures/:id' => 'pictures#update'
-
-  delete 'pictures/:id' => 'pictures#destroy'
 end
