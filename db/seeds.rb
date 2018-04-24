d1cd2c08ffa95efd4e3ce5a7b74eb81f91327e8f
Hasan Naseem <hn92@live.com>
2018-04-24 15:25:18 -0400

    db changed from sqlite to pg, for deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,46 +5,50 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
+User.create(
+  email: 'admin@admin.com',
+  password: '12345',
+  password_confirmation: '12345'
+)
 
 Picture.create(
   title:  'The old church on the coast of White sea',
   artist: 'Sergey Ershov',
   url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg',
-  user_id: 1
+  user_id: User.first.id
 )
 
 Picture.create(
   title:  'Sea Power',
   artist: 'Stephen Scullion',
   url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg',
-  user_id: 1
+  user_id: User.first.id
 )
 
 Picture.create(
   title:  'Into the Poppies',
   artist: 'John Wilhelm',
   url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg',
-  user_id: 1
+  user_id: User.first.id
 )
 
 Picture.create(
   title:  'bircage',
   artist: 'Birdcage',
   url:    'http://writingexercises.co.uk/images2/randomimage/birdcage.jpg',
-  user_id: 1
+  user_id: User.first.id
 )
 
 Picture.create(
   title:  'Tiger',
   artist: 'Emilio Koyoto',
   url:    'https://images.freeimages.com/images/large-previews/478/jack-o-lanterns-1326113.jpg',
-  user_id: 4
+  user_id: User.first.id
 )
 
 Picture.create(
   title:  'WayHigh',
   artist: 'Andrew P Cohen',
   url:    'https://images.freeimages.com/images/large-previews/478/jack-o-lanterns-1326113.jpg',
-  user_id: 1
+  user_id: User.first.id
 )
